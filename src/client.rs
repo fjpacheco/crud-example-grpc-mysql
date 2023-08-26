df1f7d0748f8c32d9b010e15b519241d5e33c1d5
Federico <fpacheco@fi.uba.ar>
2023-08-26 12:43:24 -0300

    Add get service gRPC (with mock users)

--- src/client.rs
+++ src/client.rs
@@ -1,21 +1,39 @@
-use hello_world::greeter_client::GreeterClient;
-use hello_world::HelloRequest;
+use std::env;
 
-pub mod hello_world {
-    tonic::include_proto!("helloworld");
+use dotenv::dotenv;
+use log::LevelFilter;
+use user_service::user_service_client::UserServiceClient;
+
+pub mod user_service {
+    // Note: The token passed to the include_proto macro (in our case "routeguide") is
+    // the name of the package declared in our .proto file, not a filename, e.g "routeguide.rs".
+    tonic::include_proto!("user_service");
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+    dotenv().ok();
+    env_logger::builder()
+        .filter(
+            None,
+            env::var("RUST_LOG")
+                .unwrap_or_default()
+                .parse::<LevelFilter>()
+                .unwrap_or(LevelFilter::Info),
+        )
+        .format_timestamp(None)
+        .init();
 
-    let request = tonic::Request::new(HelloRequest {
-        name: "Tonic".into(),
+    let mut client = UserServiceClient::connect("http://[::1]:50051").await?;
+    log::info!("Connected to server.");
+
+    let request_get_user = tonic::Request::new(user_service::UserId {
+        value: "1".to_string(),
     });
 
-    let response = client.say_hello(request).await?;
-
-    println!("RESPONSE={:?}", response);
+    log::info!("[1] Sending request {:?}", request_get_user);
+    let response = client.get_user(request_get_user).await?;
+    log::info!("[1] Response: {:?}", response);
 
     Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -1,39 +1,83 @@
+use dotenv::dotenv;
+use kinsper_rust_test::data::model::UserModel;
+use log::LevelFilter;
+use std::env;
+use std::sync::Arc;
 use tonic::{transport::Server, Request, Response, Status};
 
-use hello_world::greeter_server::{Greeter, GreeterServer};
-use hello_world::{HelloReply, HelloRequest};
+use user_service::user_service_server::{UserService, UserServiceServer};
+use user_service::{User, UserId};
 
-pub mod hello_world {
-    tonic::include_proto!("helloworld");
+pub mod user_service {
+    tonic::include_proto!("user_service");
 }
 
 #[derive(Default)]
-pub struct MyGreeter {}
+pub struct MyUserService {
+    mock_users: Arc<Vec<UserModel>>, // TODO! Arc ... async arc?
+}
 
 #[tonic::async_trait]
-impl Greeter for MyGreeter {
-    async fn say_hello(
-        &self,
-        request: Request<HelloRequest>,
-    ) -> Result<Response<HelloReply>, Status> {
-        println!("Got a request from {:?}", request.remote_addr());
+impl UserService for MyUserService {
+    async fn get_user(&self, request: Request<UserId>) -> Result<Response<User>, Status> {
+        log::info!("Got a request from {:?}", request.remote_addr());
 
-        let reply = hello_world::HelloReply {
-            message: format!("Hello paa {}!", request.into_inner().name),
-        };
-        Ok(Response::new(reply))
+        let user = self
+            .mock_users
+            .iter()
+            .find(|u| u.id == request.get_ref().value);
+
+        match user {
+            Some(user) => {
+                let user = User {
+                    id: Some(UserId {
+                        value: user.id.clone(),
+                    }),
+                    name: user.name.clone(),
+                    mail: user.mail.clone(),
+                };
+                Ok(Response::new(user))
+            }
+            None => Err(Status::not_found("User not found")),
+        }
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse().unwrap();
-    let greeter = MyGreeter::default();
+    dotenv().ok();
+    env_logger::builder()
+        .filter(
+            None,
+            env::var("RUST_LOG")
+                .unwrap_or_default()
+                .parse::<LevelFilter>()
+                .unwrap_or(LevelFilter::Info),
+        )
+        .format_timestamp(None)
+        .init();
 
-    println!("GreeterServer listening on {}", addr);
+    let addr = "[::1]:50051".parse().unwrap();
+
+    let users = MyUserService {
+        mock_users: Arc::new(vec![
+            UserModel {
+                id: "1".to_string(),
+                name: "Fede".to_string(),
+                mail: "fede@test.com".to_string(),
+            },
+            UserModel {
+                id: "2".to_string(),
+                name: "Abel".to_string(),
+                mail: "abel@test.com".to_string(),
+            },
+        ]),
+    };
+
+    log::info!("Listening on {}", addr);
 
     Server::builder()
-        .add_service(GreeterServer::new(greeter))
+        .add_service(UserServiceServer::new(users))
         .serve(addr)
         .await?;
 
